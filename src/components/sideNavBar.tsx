62dca20a97a9ef01484860a0b1abca90eac68471
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-12-20 11:50:51 +0000

    auto format

--- src/components/sideNavBar.tsx
+++ src/components/sideNavBar.tsx
@@ -12,22 +12,24 @@ const SideNavBar = () => {
         height: "100%",
       }}
     >
-      <Toolbar sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        marginTop: 5,
-      }}>
-          <Button sx={{color: "#c6c6c6", flexShrink: 0, marginBottom: 3}} onClick={() => navigate("/")}>
-              HOME
-          </Button>
-          <Button sx={{color: "#c6c6c6", flexShrink: 0, marginBottom: 3}} onClick={() => navigate("/user-list")}>
-              USER LIST
-          </Button>
-          <Button sx={{color: "#c6c6c6", flexShrink: 0, marginBottom: 3}} onClick={() => navigate("/message-list")}>
-              MESSAGE LIST
-          </Button>
+      <Toolbar
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          marginTop: 5,
+        }}
+      >
+        <Button sx={{ color: "#c6c6c6", flexShrink: 0, marginBottom: 3 }} onClick={() => navigate("/")}>
+          HOME
+        </Button>
+        <Button sx={{ color: "#c6c6c6", flexShrink: 0, marginBottom: 3 }} onClick={() => navigate("/user-list")}>
+          USER LIST
+        </Button>
+        <Button sx={{ color: "#c6c6c6", flexShrink: 0, marginBottom: 3 }} onClick={() => navigate("/message-list")}>
+          MESSAGE LIST
+        </Button>
       </Toolbar>
     </Box>
   );
