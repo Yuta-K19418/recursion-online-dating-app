e682fdf2917c7b190a37656e859f114162ee6831
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-12-20 11:50:40 +0000

    created search user button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,25 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-  return <div>Hello!</div>;
+  const navigate = useNavigate();
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{ flexShrink: 0 }}
+        style={{ color: "#c6c6c6" }}
+        onClick={() => navigate("/user-list")}
+      >
+        SEARCH USER
+      </Button>
+    </div>
+  );
 };
 export default Home;
