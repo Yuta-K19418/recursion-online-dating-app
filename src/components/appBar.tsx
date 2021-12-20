3986a29dce6f8e3b3efe8aa1c0752a1ae8487da8
Yuta-K19418 <yuta.k.19418@gmail.com>
2021-12-20 11:25:53 +0000

    added onclick menu movement

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -2,12 +2,24 @@ import { Box, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import MuiAppBar from "@mui/material/AppBar";
 
-const AppBar = () => {
+interface Props {
+  menuButtonState: boolean;
+  setMenuButtonState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppBar = (props: Props) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <MuiAppBar position="static" style={{ color: "#FFFFFF", backgroundColor: "#000080" }}>
+      <MuiAppBar position="static" style={{ color: "#c6c6c6", backgroundColor: "#000080" }}>
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => props.setMenuButtonState(!props.menuButtonState)}
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
